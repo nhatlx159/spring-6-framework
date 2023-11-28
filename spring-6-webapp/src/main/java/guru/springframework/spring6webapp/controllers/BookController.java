553e0f488f04e35bae2619c7256779d3aea4b4f8
nhat <nhatlx159@gmail.com>
2023-11-28 19:18:43 +0700

    add thymeleaf template and build complete services and controller

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring6webapp.controllers;
 
 import guru.springframework.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class BookController {
     private final BookService bookService;
 
